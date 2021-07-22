ded4bcb015f1f572d0f9ee1fbc8d5b6d522aa615
Ekaterina Ivanova <ekaterina_ivanova@live.com>
2021-07-22 21:42:50 +0300

    Initial commit

---
+++ src/main/java/net/iceja/JSFspringbootdemo/JsfSpringbootDemoApplication.java
@@ -0,0 +1,13 @@
+package net.iceja.JSFspringbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsfSpringbootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsfSpringbootDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/iceja/JSFspringbootdemo/JsfSpringbootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package net.iceja.JSFspringbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsfSpringbootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
